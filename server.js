775914d5bf1be8d635cce38dd9dc56ea018d918c
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-09 15:31:32 -0500

    set up express paths to directories within the public folder

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 3001;
 const indexPath = path.join(__dirname, 'Develop', 'public', 'index.html');
 const notesPath = path.join(__dirname, 'Develop', 'public', 'notes.html');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'Develop', 'public')));
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
