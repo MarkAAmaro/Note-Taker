8159c45846bdc08e65c107bf6c68c8ee7e2060c8
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-09 15:27:53 -0500

    set up npm packages nodemon,express, and uuid. set up conat variables to initiate ser.js structure

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid'); 
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
