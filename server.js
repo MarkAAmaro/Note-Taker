52cb9814289a1be596268ae7296d58cf1cbd8b9a
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-09 15:30:01 -0500

    added index and notes path to their respecitive directories and set up console.log for app listener to review issues when linking pages

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const indexPath = path.join(__dirname, 'Develop', 'public', 'index.html');
+const notesPath = path.join(__dirname, 'Develop', 'public', 'notes.html');
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
